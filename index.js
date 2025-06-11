42a35e375e8194f3f255373463a0041f38936c16
Levent Efe Çiftçi <leventefe587@gmail.com>
2025-06-11 18:28:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-require("dotenv").config();
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
-
-client.once("ready", () => {
-  console.log(`${client.user.tag} aktif!`);
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.on("guildMemberAdd", member => {
-  const kanal = member.guild.systemChannel;
-  if (kanal) {
-    kanal.send(`Hoş geldin ${member}, sunucumuza katıldığın için mutluyuz!`);
+// MongoDB bağlantısı (örnek)
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB bağlandı!'))
+  .catch(err => console.error('MongoDB hatası:', err));
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} çalışıyor!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('.kayıt')) {
+    // Kayıt işlemleri burada
   }
 });
 
 client.login(process.env.TOKEN);
+require('./keep_alive'); // Replit 7/24 aktif tutma
